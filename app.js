679a95fa51e2f60086c8beb717f0e7e2b3098e86
Bhaskar Gupta <guptabhanu1999@gmail.com>
2020-06-11 00:53:49 +0530

    Added maps feature

--- app.js
+++ app.js
@@ -15,6 +15,60 @@ var current=document.getElementById("current");
 var button=document.getElementById("submit");
 // var apikey="AIzaSyB3kAwioi5Adu1oBvr-tYIxKub_DkJ4Lx8";
 
+var map, infoWindow;
+
+var geocoder;
+var markers=[];
+function init() 
+{
+	// console.log("Init");
+  	geocoder = new google.maps.Geocoder();
+  	button.onclick=function() 
+  	{
+    geocodeAddress(geocoder);
+  	};
+  	map = new google.maps.Map(document.getElementById('map'), 
+  	{
+  		center: {lat: 28.6139, lng: 77.2090},
+  		zoom: 8
+	});
+	infoWindow = new google.maps.InfoWindow;
+}
+
+  	function geocodead(address)
+	{
+		geocoder.geocode({'address': address}, function(results, status) 
+		{
+		if (status === google.maps.GeocoderStatus.OK) 
+		{
+		// console.log("Results",results);
+			if(results.length==0)
+	    	{
+	        alert("Address not found.")
+	    	}
+	    	else
+	    	{
+	      		latitude=results[0].geometry.location.lat();
+	      		longitude=results[0].geometry.location.lng();
+	      	    var pos1 = {
+	          				lat: latitude,
+	          				lng: longitude
+	        				};
+	        	var marker = new google.maps.Marker({
+							position: pos1,
+							map: map,
+							title:'C'
+							}); 
+				markers.push(marker); 	
+	    	}
+		} 
+		else 
+		{	
+	  		alert('Geocode was not successful for the following reason: ' + status);
+		}
+		});
+	}
+
 var latitude;
 var longitude;
 function getLocation() {
@@ -30,6 +84,7 @@ function findContainment(position) {
 	// "<br>Longitude: " + position.coords.longitude;
 	latitude=position.coords.latitude;
 	longitude=position.coords.longitude;
+
 	// console.log(latitude,longitude);
 	// distance=document.getElementById('distance').value;
 	var apiurl="https://data.geoiq.io/dataapis/v1.0/covid/nearbyzones";
@@ -40,6 +95,16 @@ function findContainment(position) {
 	  "radius": 5000
 	}
 
+	var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            	};
+    DeleteMarkers();
+    infoWindow.setPosition(pos);
+    infoWindow.setContent('Location used');
+    infoWindow.open(map);
+    map.setCenter(pos);
+    map.setZoom(13);
 	// Making a POST request using an axios instance from a connected library
 	axios.post(apiurl,object)
 	  // Handle a successful response from the server
@@ -69,6 +134,17 @@ function findContainment1(latitude,longitude) {
 	  "lat": latitude,
 	  "radius": 5000
 	}
+	var pos = {
+          lat: latitude,
+          lng: longitude
+        	};
+    DeleteMarkers();
+	infoWindow.setPosition(pos);
+	infoWindow.setContent('Location used');
+	infoWindow.open(map);
+    map.setCenter(pos);
+    map.setZoom(13);
+
 	axios.post(apiurl,object)
 	  .then(response => {
 			  const data = response.data;
@@ -83,9 +159,19 @@ function findContainment1(latitude,longitude) {
 	  .catch(error => console.error('Error', error));
 }
 
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+	infoWindow.setPosition(pos);
+	infoWindow.setContent(browserHasGeolocation ?
+	                      'Error: The Geolocation service failed.' :
+	                      'Error: Your browser doesn\'t support geolocation.');
+	infoWindow.open(map);
+}
+
 function nearContainmentZone(data){
+	var i=1;
 	data.containmentZoneNames.forEach(function(zoneName){
 		ContainmentZones.innerHTML+="<li>"+zoneName+"</li>";
+		geocodead(zoneName);
 	});
 	modal1.style.display="block";
 }
@@ -106,21 +192,13 @@ current.onclick=function(){
 	getLocation();
 }
 
-function init() {
-	// console.log("Init");
-  	var geocoder = new google.maps.Geocoder();
-  	button.onclick=function() {
-    geocodeAddress(geocoder);
-  };
-}
-
 function geocodeAddress(geocoder) {
   var address = document.getElementById('address').value;
   // console.log("Address",address);
   // console.log("Called",address);
   geocoder.geocode({'address': address}, function(results, status) {
     if (status === google.maps.GeocoderStatus.OK) {
-    	console.log("Results",results);
+    	// console.log("Results",results);
     	if(results.length==0)
 	    {
 	        alert("Address not found.")
@@ -137,7 +215,13 @@ function geocodeAddress(geocoder) {
     }
   });
 }
-
+function DeleteMarkers() {
+    //Loop through all the markers and remove
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+    }
+    markers = [];
+};
 window.onclick = function(event) {
   if (event.target == modal1 || event.target == modal2) {
     modal1.style.display = "none";
